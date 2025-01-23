14be46c63b325ca2ddc142bb4e3f627130a5758a
Sualeh Siddiqui <sualehsiddiqui01@gmail.com>
2025-01-23 20:37:03 +0500

    added text on top of page

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,14 +1,26 @@
 import { Container } from "react-bootstrap";
 import "./style.css";
 import Logo from "../../Assets/logo.png";
+import { useState } from "react";
 
 
 const Navbar = () => {
 
+    const [navbar, setNavbar] = useState(false);
+
+    window.addEventListener("scroll", () => {
+        // console.log(window.scrollY, window.innerHeight)
+        if (window.scrollY > 200) {
+            setNavbar(true);
+        } else {
+            setNavbar(false);
+        }
+    });
+
     return (
         <>
             <header>
-                <div className="scrolling-text">
+                <div className={navbar ? "scrolling-text nav-scrolled" : "scrolling-text"}>
                     <p>Click on the image for a better view.</p>
                 </div>
                 <Container className="nav-conatiner">
@@ -18,7 +30,7 @@ const Navbar = () => {
                 </Container>
             </header>
         </>
-    )
-}
+    );
+};
 
 export default Navbar;
